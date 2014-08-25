a0681ac633cd1ac831daed4cb3739ac1dd0bbbd5
Martin Keckeis <martin.keckeis1@gmail.com>
2014-08-25 11:03:16 +0200

    Update Settings.php

--- Settings.php
+++ Settings.php
@@ -13,24 +13,28 @@ class Settings extends \Piwik\Plugin\Settings
     protected function init()
     {
         $setting = new SystemSetting('customVariableName', 'Name of the custom variable');
+        $setting->readableByCurrentUser = true;
         $setting->type = self::TYPE_STRING;
         $setting->defaultValue = 'username';
         $setting->inlineHelp = 'Custom variable name where you set the user identifier (e.g. username, mail)';
         $this->addSetting($setting);
         
         $setting = new SystemSetting('visitorAvatarField', 'Avatar field name');
+        $setting->readableByCurrentUser = true;
         $setting->type = self::TYPE_STRING;
         $setting->defaultValue = 'thumbnailphoto';
         $setting->inlineHelp = 'In AD it\'s normally thumbnailphoto';
         $this->addSetting($setting);
         
         $setting = new SystemSetting('visitorDescriptionFields', 'Display field name(s)');
+        $setting->readableByCurrentUser = true;
         $setting->type = self::TYPE_STRING;
         $setting->defaultValue = 'displayname';
         $setting->inlineHelp = 'In AD it\'s you can use displayname. Define multiple fields with comma. E.g. displayname,sn,givenname,mail';
         $this->addSetting($setting);
         
         $setting = new SystemSetting('searchFilter', 'LDAP search filter');
+        $setting->readableByCurrentUser = true;
         $setting->type = self::TYPE_STRING;
         $setting->defaultValue = '(&(objectclass=user)(samAccountName=%s))';
         $setting->inlineHelp = 'Search for username: "(&(objectclass=user)(samAccountName=%s))". Search for E-Mail (&(objectclass=user)(mail=%s))';
